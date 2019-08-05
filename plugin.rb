b11f7980352b34d6d941d63f468375cb93364a60
stuhin <stuhin@live.ru>
2019-08-05 22:21:57 +0700

    fix

--- plugin.rb
+++ plugin.rb
@@ -10,45 +10,41 @@ enabled_site_setting :custom_notification_enabled
 
 after_initialize do
 
-
-
-  DiscourseEvent.on(:user_updated) do |user|
+  DiscourseEvent.on(:user_first_logged_in) do |user|
     if SiteSetting.custom_notification_enabled then
       notification_type = Notification.types[:custom]
       group_users = GroupUser.where(group_id: 3);
       group_users.each do |group_user|
-      Notification.create(
-        notification_type: notification_type,
-        user_id: group_user.user_id,
-        data: {
-          message: "custom_notification",
-          display_username: "/u/#{user.username_lower}",
-          topic_title: "#{user.username} первый визит",
-        }.to_json
-      )
+        Notification.create(
+          notification_type: notification_type,
+          user_id: group_user.user_id,
+          data: {
+            message: "custom_notification",
+            display_username: "/u/#{user.username_lower}",
+            topic_title: "#{user.username} первый визит",
+          }.to_json
+        )
+      end
     end
   end
-end
 
-  DiscourseEvent.on(:user_first_logged_in) do |user|
+
+  DiscourseEvent.on(:user_created) do |user|
     if SiteSetting.custom_notification_enabled then
       notification_type = Notification.types[:custom]
-      Notification.create(
-        notification_type: notification_type,
-        user_id: user.id,
-        data: {
-          message: "custom_notification",
-          display_username: user.username,
-          description: ""
-        }.to_json
-      )
+      group_users = GroupUser.where(group_id: 3);
+      group_users.each do |group_user|
+        Notification.create(
+          notification_type: notification_type,
+          user_id: group_user.user_id,
+          data: {
+            message: "custom_notification",
+            display_username: "/admin/users/list/new",
+            topic_title: "#{user.username} зарегистрировался",
+          }.to_json
+        )
+      end
     end
   end
-
-  #DiscourseEvent.on(:user_created) do |user|
-   # if SiteSetting.custom_notification_enabled then
-   #   User.new.approve(user.id)
-   # end
- # end
 
 end
